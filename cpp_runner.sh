a6cef1001e1d60f3b274d869cfbca5e49a467e3f
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-01-09 23:22:11 +0300

    bugfix

--- cpp_runner.sh
+++ cpp_runner.sh
@@ -5,20 +5,21 @@ function cppRun() {
     # $1 - parameter of source code filename, $2 - parameter of location of the source code
     # file starting from home/gregory/MEGA/programming/c++ directory
 	SOURCE_FILE=$1.cpp;
-	if [[-f "$SOURCE_FILE"]];
+	if [[ -f "$SOURCE_FILE" ]];
 		then
 			if [ -z "$1" ]
 				then
 	   				echo "You haven't pased source code .cpp file name!";
     			else 
-            		mkdir -p /home/gregory/MEGA/programming/c++/$2/executable;
-	    			cd /home/gregory/MEGA/programming/c++/$2/executable;
-	    			g++ /home/gregory/MEGA/programming/c++/$2/$1.cpp -o executable_$1;
-    	    		./executable_$1; 
+            		mkdir -p executable;
+	    			cd executable;
+	    			g++ ../$1.cpp -o executable_$1;
+    	    		./executable_$1;
+    	    		cd ..; 
     		fi
     	else
     		echo "In current directory there is no needed source code .cpp file!";
 	
 	fi
-    
 }
+
